8a597a4e0a2ed0859b2e336b8a50a7b4c2687e2a
Tomas Balyo <biotomas@gmail.com>
2019-01-26 18:31:36 +0100

    dialog and room changes

---
+++ Assets/GameMasterScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMasterScript : MonoBehaviour
+{
+    public static GameMasterScript instance;
+
+    public Camera[] cameras;
+
+    public void changeCameraTo(int index) {
+        for (int i = 0; i < cameras.Length; i++) {
+            if (i == index) {
+                cameras[i].enabled = true;
+            } else {
+                cameras[i].enabled = false;
+            }
+        }
+    }
+
+    private HashSet<string> flags;
+
+    public void Start() {
+        instance = this;
+        flags = new HashSet<string>();
+    }
+    public bool getFlag(string name) {
+        return flags.Contains(name);
+    }
+
+    public void setFlag(string name) {
+        flags.Add(name);
+    }
+
+}
