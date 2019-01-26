1e7f7ef9e8e6da6e41a4c43daa3a57306e858d07
Dominik Schreiber <mail@dominikschreiber.de>
2019-01-26 23:18:38 +0100

    Merge new title screen with inventory stuff

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+// Quits the player when the user hits escape
+
+public class QuitApplication : MonoBehaviour
+{
+    void OnMouseUp()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
