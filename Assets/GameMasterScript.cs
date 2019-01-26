980e2af740992fbce66404091be380eab9033d6f
Patrick Hegemann <p.hegemann@mailbox.org>
2019-01-26 18:54:47 +0100

    start in scene 0

--- Assets/GameMasterScript.cs
+++ Assets/GameMasterScript.cs
@@ -23,6 +23,7 @@ public class GameMasterScript : MonoBehaviour
     public void Start() {
         instance = this;
         flags = new HashSet<string>();
+        changeCameraTo(0);
     }
     public bool getFlag(string name) {
         return flags.Contains(name);
