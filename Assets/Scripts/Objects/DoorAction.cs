fa051b1ab75999deea61a3b109351067d4d45c05
Patrick Hegemann <p.hegemann@mailbox.org>
2019-01-27 11:47:47 +0100

    Tech works

---
+++ Assets/Scripts/Objects/DoorAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Objects
+{
+    public class DoorAction : ActionHandler
+    {
+        public DialogSpawner dialogSpawner;
+        public int which;
+
+        public override void HandleAction()
+        {
+            if (which == 3 && GameMasterScript.instance.getFlag("-c-openDoor1")) {
+                Debug.Log("entering bathroom");
+                GameMasterScript.instance.changeCameraTo(1);
+                return;
+            }
+            dialogSpawner.startDialog(which);
+        }
+    }
+}
